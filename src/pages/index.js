60938fc4392c5ede2297b56de5180105bd18d113
Sangam Kumar <sangamcse.dev@gmail.com>
2020-11-11 20:34:19 +0530

    Fix PC Load Letter Error
    Signed-off-by: Sangam Kumar <sangamcse.dev@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,21 +7,8 @@ function Index() {
       <MyHead />
       <div className="w-screen p-12 flex flex-col">
         <h1 className="text-center text-heading text-4xl font-bold">
-          NextJS + Tailwind Theming example
+          Sangam Kumar
         </h1>
-        <p className="text-center mt-8 text-xl text-body">
-          This is a basic example of how to setup theming using Tailwind +
-          PostCSS. checkout the{' '}
-          <a
-            className="text-primary hover:text-primary-hover"
-            href="https://github.com/sangamcse"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            repo
-          </a>{' '}
-          for more information!
-        </p>
       </div>
     </Layout>
   );
