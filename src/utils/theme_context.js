8ca003b73465c84234b35c40b66f42b2fdd77dd5
Sangam Kumar <sangamcse.dev@gmail.com>
2021-01-26 01:13:58 +0530

    Fixed mispeling
    Signed-off-by: Sangam Kumar <sangamcse.dev@gmail.com>

--- src/utils/theme_context.js
+++ src/utils/theme_context.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const initialValue = { theme: 'theme-light' };
 
@@ -6,6 +6,18 @@ export const ThemeContext = createContext(initialValue);
 
 export default function ThemeProvider({ children }) {
   const [theme, setTheme] = useState('theme-light');
+
+  useEffect(() => {
+    function handleTheme() {
+      const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+      if (isDark) {
+        setTheme('theme-dark');
+      }
+    }
+
+    handleTheme();
+  }, []);
 
   function toggleTheme() {
     setTheme(theme === 'theme-light' ? 'theme-dark' : 'theme-light');
