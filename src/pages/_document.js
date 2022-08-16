567c26e27cd0edeec6ee20c82b106694ae3f0262
Sangam Kumar <sangamcse.dev@gmail.com>
2022-08-16 16:08:09 +0530

    fix: Update DM Mono font
    Signed-off-by: Sangam Kumar <sangamcse.dev@gmail.com>

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,11 +11,9 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link
-            as="style"
             rel="stylesheet preload prefetch"
             href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;400;500&display=swap"
-            type="text/css"
-            crossOrigin="anonymous"
+            as="style"
           />
         </Head>
         <body>
