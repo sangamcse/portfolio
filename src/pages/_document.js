299a7206c463b6b187d78d716c3b50a76a2f0d51
Sangam Kumar <sangamcse.dev@gmail.com>
2022-09-10 13:44:22 +0530

    third time's a charm
    Signed-off-by: Sangam Kumar <sangamcse.dev@gmail.com>

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,12 +11,37 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* Google Tag Manager */}
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5KNNPZS');
+              `,
+            }}
+          ></Script>
+          {/* End Google Tag Manager */}
           <link
             href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;400;500&display=swap"
             rel="stylesheet"
           />
         </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5KNNPZS"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
+          {/* End Google Tag Manager (noscript) */}
           <Main />
           <NextScript />
         </body>
