7a62019aca384b2070aa93f7d3d62baa1d3c364f
Sangam Kumar <sangamcse.dev@gmail.com>
2022-09-11 13:50:39 +0530

    I am the greatest javascript developer in the world.
    Signed-off-by: Sangam Kumar <sangamcse.dev@gmail.com>

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,19 +12,15 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {/* Google Tag Manager */}
-          <Script
-            id="google-analytics"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-5KNNPZS');
-              `,
-            }}
-          ></Script>
+            `}
+          </Script>
           {/* End Google Tag Manager */}
           <link
             href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;400;500&display=swap"
