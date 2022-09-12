e8516f39ac26a39b0f3baf9f6f1de7ccb7bfacb8
Sangam Kumar <sangamcse.dev@gmail.com>
2022-09-12 17:54:00 +0530

    doh.
    Signed-off-by: Sangam Kumar <sangamcse.dev@gmail.com>

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,24 @@
-export const About = () => {
-  return (
-    <main className="m-auto py-8 max-w-readability-lg prose lg:prose-lg xl:prose-xl">
-      <article>
-        <div className="px-4">
-          <h1>About</h1>
-          <p className="text-body">
-            Hi! My name is Sangam. I&apos;m from Bihar, and I&apos;m a Full
-            Stack Developer working as a Senior Software Engineer at{' '}
-            <a href="https://www.hackerrank.com">HackerRank</a> in Bangalore,
-            India. We build tools to help recruiters hire developers.
-          </p>
-          <p className="text-body">
-            I love computers and enjoy writing code. I&apos;m particularly
-            interested in backend development, how the system works, its
-            reliability, and scalability. Apart from computers, I enjoy reading,
-            traveling, walking, cooking, and music. If you have any questions or
-            just want to chat, you can reach out to me on{' '}
-            <a href="https://www.twitter.com/meetsangamcse">Twitter</a> or shoot
-            me an <a href="mailto:sangamcse.dev@gmail.com">email</a>.
-          </p>
-        </div>
-      </article>
-    </main>
-  );
-};
+export const About = () => (
+  <main className="m-auto py-8 max-w-readability-lg prose lg:prose-lg xl:prose-xl">
+    <article>
+      <div className="px-4">
+        <h1>About</h1>
+        <p className="text-body">
+          Hi! My name is Sangam. I&apos;m from Bihar, and I&apos;m a Full Stack
+          Developer working as a Senior Software Engineer at{' '}
+          <a href="https://www.hackerrank.com">HackerRank</a> in Bangalore,
+          India. We build tools to help recruiters hire developers.
+        </p>
+        <p className="text-body">
+          I love computers and enjoy writing code. I&apos;m particularly
+          interested in backend development, how the system works, its
+          reliability, and scalability. Apart from computers, I enjoy reading,
+          traveling, walking, cooking, and music. If you have any questions or
+          just want to chat, you can reach out to me on{' '}
+          <a href="https://www.twitter.com/meetsangamcse">Twitter</a> or shoot
+          me an <a href="mailto:sangamcse.dev@gmail.com">email</a>.
+        </p>
+      </div>
+    </article>
+  </main>
+);
