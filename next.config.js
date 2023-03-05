bf74c3056d06d767cd0b70cca378726e98c6c3f5
Sangam Kumar <sangamcse.dev@gmail.com>
2023-03-05 14:46:31 +0530

    Change feed to load in webpack
    Signed-off-by: Sangam Kumar <sangamcse.dev@gmail.com>

--- next.config.js
+++ next.config.js
@@ -10,4 +10,20 @@ module.exports = {
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && isServer) {
+      const originalEntry = config.entry;
+
+      config.entry = async () => {
+        const entries = { ...(await originalEntry()) };
+
+        // These scripts can import components from the app and use ES modules
+        entries['scripts/generate-rss.js'] = './scripts/generate-rss.js';
+
+        return entries;
+      };
+    }
+
+    return config;
+  },
 };
