f0e4ff17be41ff172a40c8e7204e8c717db1e9d7
Sangam Kumar <sangamcse.dev@gmail.com>
2023-03-06 20:18:16 +0530

    fixup! fixup! Change feed to load in webpack

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -5,15 +5,14 @@ import { MyHead } from 'components/SiteHead';
 import Layout from 'components/Layout';
 import { Blog } from 'components/blog/blog';
 
-import { getFrontMatters } from 'utils/get_front_matters';
-import { BLOG_PATH_PREFIX } from 'utils/mdx';
+import { generateRssFeed } from 'utils/generate_rss_feed';
 
 const title = 'Blog | sangamcse';
 const description =
   "Sangam's thoughts on software engineering, travelling, photography and many more subjects, in written form!";
 
-export const getStaticProps = () => ({
-  props: { pages: getFrontMatters(`${BLOG_PATH_PREFIX}**/*.mdx`) },
+export const getStaticProps = async () => ({
+  props: { pages: await generateRssFeed() },
 });
 
 function Index({ pages }) {
--- src/utils/generate_rss_feed.js
+++ src/utils/generate_rss_feed.js
@@ -1,46 +1,83 @@
 import fs from 'fs';
+import ReactDOMServer from 'react-dom/server';
+import React, { Fragment } from 'react';
 import { Feed } from 'feed';
+import { MDXRemote } from 'next-mdx-remote';
+import { serialize } from 'next-mdx-remote/serialize';
 
 import { getFrontMatters } from 'utils/get_front_matters';
-import { BLOG_PATH_PREFIX } from 'utils/mdx';
+import { BLOG_PATH_PREFIX, blogComponents, mdxOptions } from 'utils/mdx';
 import { rootUrl } from 'utils/constants';
 
-export default async function generateRssFeed() {
+export async function generateRssFeed() {
   const allPosts = await getFrontMatters(`${BLOG_PATH_PREFIX}**/*.mdx`);
 
   const feedOptions = {
-    title: 'Blog posts | RSS Feed',
-    description: 'Welcome to my blog posts!',
+    title: 'Sangam Kumar',
+    description: 'My most recent blog posts',
     id: rootUrl,
     link: rootUrl,
     image: 'https://avatars.githubusercontent.com/u/26381717',
     favicon: `${rootUrl}/favicon.ico`,
-    copyright: `All rights reserved ${new Date().getFullYear()}, Sangam`,
-    generator: `Feed for Node.js`,
     feedLinks: {
-      rss2: `${rootUrl}/rss.xml`,
-      json: `${rootUrl}/rss.json`,
-      atom: `${rootUrl}/atom.xml`,
+      json: `${rootUrl}/json`,
+      rss2: `${rootUrl}/rss`,
     },
+    generator: rootUrl,
+    author: {
+      name: 'Sangam Kumar',
+      email: 'sangamcse.dev@gmail.com',
+      link: rootUrl,
+    },
+    copyright: `All rights reserved ${new Date().getFullYear()}, Sangam Kumar`,
   };
 
   const feed = new Feed(feedOptions);
 
-  allPosts.forEach((post) => {
-    feed.addItem({
-      title: post.title,
-      id: `${rootUrl}/${post.slug}`,
-      link: `${rootUrl}/${post.slug}`,
-      description: post.snippet,
-      date: new Date(post.publishedAt),
-      published: new Date(post.publishedAt),
-      image: `${rootUrl}/${post.img}`,
-      content: post.content,
-      category: post.tags.map((tag) => ({ name: tag })),
-    });
-  });
+  const entries = await Promise.all(
+    allPosts.slice(0, 10).map(async (post) => {
+      const { content, title, slug, img, publishedAt, tags, snippet } = post;
 
-  fs.writeFileSync('./public/rss.xml', feed.rss2());
-  fs.writeFileSync('./public/rss.json', feed.json1());
-  fs.writeFileSync('./public/atom.xml', feed.atom1());
+      const source = await serialize(content, {
+        scope: { title, slug, img, publishedAt, tags, snippet },
+        components: blogComponents,
+        mdxOptions,
+      });
+
+      return {
+        ...post,
+        content: source,
+      };
+    })
+  );
+
+  entries.forEach(
+    ({ title, slug, img, publishedAt, tags, snippet, content }) => {
+      feed.addItem({
+        title,
+        id: `${rootUrl}/${slug}`,
+        link: `${rootUrl}/${slug}`,
+        date: new Date(publishedAt),
+        published: new Date(publishedAt),
+        image: `${rootUrl}/${img}`,
+        description: snippet,
+        category: tags.map((tag) => ({ name: tag })),
+        content: ReactDOMServer.renderToStaticMarkup(
+          <Fragment>
+            <MDXRemote {...content} components={blogComponents} />
+            <a
+              href={`mailto:sangamcse.dev@gmail.com?subject=Reply%20to:%20“${title}”`}
+            >
+              Reply via e-mail
+            </a>
+          </Fragment>
+        ),
+      });
+    }
+  );
+
+  fs.writeFileSync('./public/feed.xml', feed.rss2());
+  fs.writeFileSync('./public/feed.json', feed.json1());
+
+  return allPosts;
 }
